4e438767128a1efc4ee567f6e2f3b2cfd207e493
AryanSoni27 <aryansoni33635@gmail.com>
2025-02-25 19:35:53 +0530

    Write some code

--- src/main/java/com/chat/app/Controller/RoomController.java
+++ src/main/java/com/chat/app/Controller/RoomController.java
@@ -1,4 +1,20 @@
 package com.chat.app.Controller;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+
+    //Create Room
+
+
+
+    //Get Room By RoomId
+
+
+
+    //Get Messages of Room
 }
--- src/main/java/com/chat/app/Service/RoomService.java
+++ src/main/java/com/chat/app/Service/RoomService.java
@@ -1,4 +1,31 @@
 package com.chat.app.Service;
 
+import com.chat.app.Entity.Room;
+import com.chat.app.Repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomService {
+    @Autowired
+    private RoomRepository roomRepository;
+    //Create New Room
+    private ResponseEntity<?> createRoom(Room room){
+
+        if(roomRepository.findByRoomId(room.getRoomId()) != null){
+            return ResponseEntity.badRequest().body("Room already exists");
+        }
+        roomRepository.save(room);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    //Get Room by RoomId
+    private Room getRoomById(String id){
+        return roomRepository.findByRoomId(id);
+    }
+
+    //Get messages of the room
+
 }
